a0fd5892232ba6164ba6db6507e3b02151bbb066
Vaman Deshmukh <vamandeshmukh@gmail.com>
2020-09-26 07:09:38 +0530

    First commit

---
+++ jenkins-demo-app/src/main/java/jenkins/demo/app/App.java
@@ -0,0 +1,12 @@
+package jenkins.demo.app;
+/**
+ * 
+ * @author Vaman Deshmukh 
+ *
+ */
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
